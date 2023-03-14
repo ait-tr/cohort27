d8a92615d6463880de784a798724f90699d476a1
Marsel Sidikov <sidikov.marsel@gmail.com>
2023-03-15 00:16:39 +0300

    add lesson_01 materials

---
+++ basic_programming/lesson_01/code/hello_world/src/Main1.java
@@ -0,0 +1,7 @@
+public class Main1 {
+  public static void main(String args[]) {
+    System.out.println("Hello, Marsel!");
+    System.out.println("Bye, Java!");
+    System.out.println("C/C++ is very cool!");
+  }
+}
