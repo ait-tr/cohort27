89b33c9280b82740a5eb4874ed551fbccd9cf947
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-07-03 21:23:27 +0200

    feat: add output

--- basic_programming/lesson_42/code/array_lists/src/Task1.java
+++ basic_programming/lesson_42/code/array_lists/src/Task1.java
@@ -26,11 +26,23 @@ public class Task1 {
     int amount = scanner.nextInt();
     System.out.println("Введите " + amount + " чисел:");
     for (int i = 0; i < amount; ++i) { // повтори amount раз
-      int x = scanner.nextInt();
-      numbers.add(x);
+//      int x = scanner.nextInt();
+//      numbers.add(x);
+      numbers.add(scanner.nextInt());
     }
     // отладочный вывод - вывод промежуточного результата
     // перед сдачей программы его необходимо удалить или закомментировать
 //    System.out.println("[DEBUG] Получившийся список: " + numbers);
+
+    // - вывести только элементы с чётными индексами
+    // проверка на чётность:
+    // чётное = делится на 2 (кратное двум)
+    // делится = делится без остатка = остатка от деления нет = остаток равен нулю
+    for (int i = 0; i < numbers.size(); ++i) { // List.size() - размер списка
+      if (i % 2 == 0) { // если индекс чётный
+        System.out.println(numbers.get(i)); // List.get(индекс) - получить элемент по индексу
+      }
+    }
+    // бонус: цикл с шагом +2: `for (int i = 0; i < numbers.size(); i += 2)`
   }
 }
