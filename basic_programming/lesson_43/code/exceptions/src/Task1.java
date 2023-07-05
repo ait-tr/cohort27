5ea27ddf0c5b2bdb2270f8e156b262782cf606c6
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-07-05 19:39:12 +0200

    feat: add name replacement

--- basic_programming/lesson_43/code/exceptions/src/Task1.java
+++ basic_programming/lesson_43/code/exceptions/src/Task1.java
@@ -25,6 +25,29 @@ public class Task1 {
       names.add(scanner.nextLine());
     }
     printNumberedList(names);
+
+    System.out.print("Введите номер имени, которое хотите заменить: ");
+    int ordToRemove = scanner.nextInt(); // читаем номер от 1 до size
+    scanner.nextLine(); // дочитываем до конца строки
+    // нужен индекс от 0 до (size - 1)
+    System.out.println("Старое имя: " + names.get(ordToRemove - 1));
+    // удалим старое имя
+    names.remove(ordToRemove - 1); // номер превращаем в индекс
+    // прочитаем новое имя
+    System.out.print("Введите новое имя: ");
+    String name = scanner.nextLine();
+    // добавим новое имя на то же место
+    // в методе add указывается НОВЫЙ, желаемый индекс вставляемого элемента
+    names.add(ordToRemove - 1, name);
+    // добавление в конец: какой индекс?
+    // предположим, что в списке 3 элемента (индексы от 0 до 2, size == 3)
+    //                            0 1 2 3 4
+    //                            A B C
+    // * size - 1: (3 - 1) = 2,   A B N C
+    // * size:     3,             A B C N
+    // * size + 1: 4,             A B C - ошибка!!!
+    System.out.println("Имена после замены:");
+    printNumberedList(names);
   }
 
   private static void printNumberedList(List<String> words) {
