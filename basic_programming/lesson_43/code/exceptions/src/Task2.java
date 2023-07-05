faab4f4a67e47c1b9dcc14e1a23f9754c296d75b
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-07-05 20:00:36 +0200

    fix: replace `remove` and `add` to `set`

--- basic_programming/lesson_43/code/exceptions/src/Task2.java
+++ basic_programming/lesson_43/code/exceptions/src/Task2.java
@@ -13,8 +13,7 @@ public class Task2 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Введите количество имён: ");
-    int amount = scanner.nextInt(); // если после этого в строке точно ничего не будет,
-    // и мы хотим дочитать до конца строки, чтобы сканер был в "рабочем" состоянии:
+    int amount = scanner.nextInt();
     scanner.nextLine();
     List<String> names = new ArrayList<>();
     System.out.println("Введите " + amount + " имён, каждое с новой строки:");
@@ -24,26 +23,14 @@ public class Task2 {
     printNumberedList(names);
 
     System.out.print("Введите номер имени, которое хотите заменить: ");
-    int ordToRemove = scanner.nextInt(); // читаем номер от 1 до size
-    scanner.nextLine(); // дочитываем до конца строки
-    // нужен индекс от 0 до (size - 1)
-    int iToRemove = ordToRemove - 1;
-    System.out.println("Старое имя: " + names.get(iToRemove));
-    // удалим старое имя
-    names.remove(iToRemove); // номер превращаем в индекс
-    // прочитаем новое имя
+    int ordToReplace = scanner.nextInt(); // читаем номер от 1 до size
+    scanner.nextLine();
+    int iToReplace = ordToReplace - 1; // нужен индекс от 0 до (size - 1)
+    System.out.println("Старое имя: " + names.get(iToReplace));
     System.out.print("Введите новое имя: ");
     String name = scanner.nextLine();
-    // добавим новое имя на то же место
-    // в методе add указывается НОВЫЙ, желаемый индекс вставляемого элемента
-    names.add(iToRemove, name);
-    // добавление в конец: какой индекс?
-    // предположим, что в списке 3 элемента (индексы от 0 до 2, size == 3)
-    //                            0 1 2 3 4
-    //                            A B C
-    // * size - 1: (3 - 1) = 2,   A B N C
-    // * size:     3,             A B C N
-    // * size + 1: 4,             A B C - ошибка!!!
+    names.set(iToReplace, name); // List.set(index, element) - задание нового значения
+    // ссылка на старый элемент при этом просто исчезает (затирается)
     System.out.println("Имена после замены:");
     printNumberedList(names);
   }
