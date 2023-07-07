5d592ee7f0731819421102d82c88757f940c1d34
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-07-07 18:57:08 +0200

    feat: add initial description

--- basic_programming/lesson_44/code/practice/src/Main.java
+++ basic_programming/lesson_44/code/practice/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
 
+  // MVP - Minimal Valuable Product
+  // Кассовый аппарат:
+  // - получает информацию о позициях
+  // - закрывает чек и печатает его (на экран)
+  // Чек:
+  // - номер
+  // - порядковый номер, наименование позиции, стоимость
+  // - сумма
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
