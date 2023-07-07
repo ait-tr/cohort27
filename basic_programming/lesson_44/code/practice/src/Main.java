27fc57aeb4025779d1d7be97299d1845cd6147b0
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-07-07 19:44:50 +0200

    feat: add Main template

--- basic_programming/lesson_44/code/practice/src/Main.java
+++ basic_programming/lesson_44/code/practice/src/Main.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
+
+  // final означает, что переменную нельзя изменить
+  // (она больше не переменная, а постоянная - константа)
+  // нельзя изменить - нельзя нигде больше написать `ADD = новое значение`
+  private static final int ADD = 1;
+  private static final int CLOSE = 2;
+  private static final int EXIT = 0;
 
   // MVP - Minimal Valuable Product
   // Кассовый аппарат:
@@ -9,6 +18,25 @@ public class Main {
   // - порядковый номер, наименование позиции, стоимость
   // - сумма
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("=== Кассовый аппарат v. 0.0.1 ===");
+    Register register = new Register();
+    int command;
+    do {
+      command = readCommand(scanner);
+      System.out.println("Вы ввели команду " + command);
+    } while (command != EXIT);
+    System.out.println("До свидания!");
+  }
+
+  private static int readCommand(Scanner scanner) {
+    System.out.println("Команды:");
+    System.out.println(ADD + ". Добавить позицию");
+    System.out.println(CLOSE + ". Закрыть чек"); // автоматически начнётся новый
+    System.out.println(EXIT + ". Выход");
+    System.out.println("Выберите команду:");
+    int command = scanner.nextInt();
+    scanner.nextLine();
+    return command;
   }
 }
