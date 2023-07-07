e0fb20daff7dadea143d2da1c748c59b786e1bab
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-07-07 21:57:07 +0200

    feat: implement Bill.addInteractive()

--- basic_programming/lesson_44/code/practice/src/Bill.java
+++ basic_programming/lesson_44/code/practice/src/Bill.java
@@ -1,9 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Bill {
 
-  public void addInteractive(Scanner scanner) {
+  List<Row> rows = new ArrayList<>();
 
+  public void addInteractive(Scanner scanner) {
+    System.out.print("Введите название позиции: ");
+    String title = scanner.nextLine();
+
+    System.out.print("Введите стоимость: ");
+    while (!scanner.hasNextDouble()) { // пока следующее, что сканер может прочитать - не число
+      String errorLine = scanner.nextLine();
+      System.out.println("Некорректный ввод, введите число: " + errorLine);
+    } // это закончится, когда следующее, что прочитает сканер - число
+    double price = scanner.nextDouble(); // значит, команда отработает без ошибок
+    scanner.nextLine(); // дочитать строку до конца
+
+    rows.add(new Row(title, price));
   }
 
   @Override
---
+++ basic_programming/lesson_44/code/practice/src/Row.java
@@ -0,0 +1,10 @@
+public class Row {
+
+  private String title;
+  private double price;
+
+  public Row(String title, double price) {
+    this.title = title;
+    this.price = price;
+  }
+}
