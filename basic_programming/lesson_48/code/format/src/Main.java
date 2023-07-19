5425756eb711ef3c7c67d83b87540d152e3821a2
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-07-19 18:55:35 +0200

    feat: add printf example

--- basic_programming/lesson_48/code/format/src/Main.java
+++ basic_programming/lesson_48/code/format/src/Main.java
@@ -1,6 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // printf(формат, аргументы для подстановки)
+    // аргументы для подстановки в строке формата начинаются с %
+    // %d - decimal  - целые числа   (int, long...)
+    // %f - float    - дробные числа (double, float...)
+    // %s - string   - строки        (String)
+    // %n - new line - символ конца строки
+    String name = "Kirill";
+    Map<String, String> greeting = new HashMap<>();
+    greeting.put("en", "Hello %s!%n");
+    greeting.put("ru", "Привет, %s!%n");
+    System.out.printf(greeting.get("en"), name);
+
+    int age = 30;
+    System.out.printf("I'm %d years old!%n", age);
+
+    double pi = 3.1415;
+    // %.(знаковПослеТочки)f
+    System.out.printf("pi = %.2f%n", pi);
+
+    // %(знаков)d/s/f - дополнит слева пробелами до нужного количества,
+    // если символов и так больше, выведет "как есть" (символы не пропадут)
+    System.out.printf("Names:      %10s %10s%n", "Alice", "Bob");
+    System.out.printf("Last names: %10s %10s%n", "Dow", "Dow");
   }
 }
