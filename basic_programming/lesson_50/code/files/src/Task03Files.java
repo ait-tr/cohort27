04578406ce41d0311bc1cc9ba086d0b085ce0c7b
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-07-21 22:04:13 +0200

    feat: add output in Task03Files

--- basic_programming/lesson_50/code/files/src/Task03Files.java
+++ basic_programming/lesson_50/code/files/src/Task03Files.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Task03Files {
@@ -7,7 +9,7 @@ public class Task03Files {
   // Прочитать из файла res/input.txt имена и накопленные баллы.
   // Вывести студента с наибольшим количеством баллов
   // в файл res/output.txt.
-  public static void main(String[] args) /*throws FileNotFoundException*/ {
+  public static void main(String[] args) throws IOException {
     // Класс File используется для хранения информации
     // о файле. Информация может быть некорректной.
 //    File input = new File("res/input.txt");
@@ -34,7 +36,18 @@ public class Task03Files {
         resultName = name; // а это новый "лучший ученик"
       }
     }
+    scanner.close(); // закрываем файл - прочитали все данные
 
-    System.out.println(resultName + " (score: " + resultScore + ")");
+    File output = new File("res/output.txt");
+    // FileWriter - класс для записи данных в файл
+    FileWriter writer;
+    try {
+      writer = new FileWriter(output);
+    } catch (IOException e) {
+      System.err.println("Не получилось открыть файл для записи:" + e.getMessage());
+      return;
+    }
+    writer.write(resultName + "\n");
+    writer.close(); // закрываем файл - записали все данные
   }
 }
