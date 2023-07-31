b7a5b011b5ca54d23361254b94fcc226f5d4d2b0
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-07-31 18:46:00 +0200

    feat: add pizza list

--- basic_programming/lesson_54/code/comparator/src/Main.java
+++ basic_programming/lesson_54/code/comparator/src/Main.java
@@ -1,6 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    List<Pizza> pizzas = new ArrayList<>();
+    pizzas.add(new Pizza("A", "L", 25));
+    pizzas.add(new Pizza("A", "S", 17));
+    pizzas.add(new Pizza("B", "M", 9));
+    pizzas.add(new Pizza("A", "XS", 15));
+
+    System.out.println("До сортировки:");
+    for (Pizza p : pizzas) {
+      System.out.println(p);
+    }
+    Collections.sort(pizzas);
+    System.out.println("После сортировки:");
+    for (Pizza p : pizzas) {
+      System.out.println(p);
+    }
   }
 }
