58b96444b9e8d366ec1bf6851ab03d9595aa81fd
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-02 18:53:26 +0200

    feat: add homework_50.task1 template

--- basic_programming/lesson_55/code/tests/src/homework_50/Task1.java
+++ basic_programming/lesson_55/code/tests/src/homework_50/Task1.java
@@ -1,5 +1,11 @@
 package homework_50;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Task1 {
 
   // Напишите программу, которая:
@@ -10,6 +16,31 @@ public class Task1 {
   //
   // Воспользуйтесь методом `Integer.toBinaryString()`.
   public static void main(String[] args) {
+    String inputFileName = "res/in.txt";
+    File inputFile = new File(inputFileName);
+    Scanner scanner;
+    try {
+      scanner = new Scanner(inputFile);
+    } catch (FileNotFoundException e) {
+      // у логов часто есть уровни: DEBUG, INFO, WARNING, ERROR, CRITICAL
+      // логи ошибок: "УРОВЕНЬ: время: ошибка: ввод (причина): детали"
+      System.err.println("Ошибка при чтении из файла: " + inputFileName + ": " + e.getMessage());
+      return; // выход из программы
+    }
+    // TODO чтение данных из scanner
+    scanner.close();
 
+    String result = "";
+    // TODO перевод в двоичную систему
+
+    String outputFileName = "res/out.txt";
+    File outputFile = new File(outputFileName);
+    try {
+      FileWriter writer = new FileWriter(outputFile);
+      // TODO запись данных в writer
+      writer.close();
+    } catch (IOException e) {
+      System.err.println("Ошибка при записи в файл: " + outputFileName + ": " + e.getMessage());
+    }
   }
 }
