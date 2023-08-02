7a508c393fb0067ca92279f50a0412d12224b843
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-02 20:01:02 +0200

    feat: add homework_53.Book constructor

--- basic_programming/lesson_55/code/tests/src/homework_53/Book.java
+++ basic_programming/lesson_55/code/tests/src/homework_53/Book.java
@@ -10,4 +10,23 @@ public class Book {
   // а если авторы совпадают - по названиям
   // (и там, и там - по алфавиту, "в словарном порядке"),
   // а если и авторы совпадают - по количеству страниц (по возрастанию).
+  private final String author;
+  private final String title;
+  private final int pages;
+
+  public Book(String author, String title, int pages) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+  }
+
+  // не обязательно для решения, но обязательно для проверки
+  @Override
+  public String toString() {
+    return "Book{" +
+        "author='" + author + '\'' +
+        ", title='" + title + '\'' +
+        ", pages=" + pages +
+        '}';
+  }
 }
