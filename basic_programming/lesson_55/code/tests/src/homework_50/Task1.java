2b7530fef1d716e0ae82f59ae2fc91528b72cccb
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-02 18:33:32 +0200

    feat: add homework_50.task1 description

---
+++ basic_programming/lesson_55/code/tests/src/homework_50/Task1.java
@@ -0,0 +1,15 @@
+package homework_50;
+
+public class Task1 {
+
+  // Напишите программу, которая:
+  //
+  // - прочитает целое число из файла `res/in.txt`
+  // - переведёт его в двоичную запись
+  // - запишет его в файл `res/out.txt`
+  //
+  // Воспользуйтесь методом `Integer.toBinaryString()`.
+  public static void main(String[] args) {
+
+  }
+}
