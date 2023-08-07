ef30f8cff6f65a82e85f665da038b390f61a0d43
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-07 21:15:56 +0200

    feat: add date and time examples

--- basic_programming/lesson_57/code/dates/src/Main.java
+++ basic_programming/lesson_57/code/dates/src/Main.java
@@ -1,3 +1,8 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
 public class Main {
 
   public static void main(String[] args) {
@@ -9,6 +14,25 @@ public class Main {
     // - сравнения с приоритетом (отличаются и авторы, и названия, но "в разные стороны")
     // - один и тот же объект сам с собой
     // - что-то и null (throws NullPointerException)
-    System.out.println("Hello world!");
+
+    // новая тема: работа с датами в Java
+    // первый вариант: java.util.Date (устарел с выходом Java 8)
+    // второй вариант: java.util.Calendar (заменил часть методов Data и используется в связке с ним)
+    // третий вариант: пакет java.time (появился в Java 8)
+
+    // java.time:
+    // - LocalDate - хранит только дату без учёта момента времени (5-е августа 2023 г.)
+    // - LocalTime - хранит только время без учёта даты (15:37:24) и тайм-зоны
+    // - LocalDateTime - хранит и дату, и время
+    // - ZonedDateTime - хранит и дату, и время, и тайм-зону
+    // У всех этих классов есть статический метод .now(), возвращающий текущий момент времени,
+    //  и статические методы .of(), работающие как конструкторы
+    System.out.println("Сейчас: " + LocalTime.now().withNano(0));
+    System.out.println("12:34 : " + LocalTime.of(12, 34));
+
+    System.out.println("Сегодня : " + LocalDate.now());
+    System.out.println("1.2.2003: " + LocalDate.of(2003, Month.FEBRUARY, 1));
+
+    System.out.println("Сегодня и сейчас: " + LocalDateTime.now());
   }
 }
