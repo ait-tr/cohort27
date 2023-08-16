6ea56d6061916f105a2ba547b725efeda34afdef
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-16 20:06:10 +0200

    fix: optimize Task1 memory usage

--- basic_programming/lesson_61/code/collections/src/Task1.java
+++ basic_programming/lesson_61/code/collections/src/Task1.java
@@ -4,16 +4,13 @@ public class Task1 {
 
   // Вводят количество чисел, а затем и сами числа.
   // Найти и вывести сумму всех чисел
-  public static void main(String[] args) {    // time = O(n)    space = O(n)
+  public static void main(String[] args) {    // time = O(n)    space = O(1)
     Scanner scanner = new Scanner(System.in); // O(1)           O(1)
     int size = scanner.nextInt();             // O(1)           O(1)
-    int[] array = new int[size];              // O(1)           O(n)
-    for (int i = 0; i < size; i++) {          // n раз = O(n)   +O(1)
-      array[i] = scanner.nextInt();           // - O(1)
-    }
     int sum = 0;                              // O(1)           O(1)
-    for (int i = 0; i < array.length; i++) {  // n раз = O(n)   +O(1)
-      sum += array[i];                        // - O(1)
+    for (int i = 0; i < size; i++) {          // n раз = O(n)   +O(1)
+      int x = scanner.nextInt();              // - O(1)          +O(1)
+      sum += x;                               // - O(1)
     }
     System.out.println(sum);                  // O(1)
   }
