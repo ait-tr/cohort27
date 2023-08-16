600898bd21bd8ef3b1cd758b7a317371153c7b2f
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-16 19:30:45 +0200

    feat: add Task1 description

---
+++ basic_programming/lesson_61/code/collections/src/Task1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Task1 {
+
+  // Вводят количество чисел, а затем и сами числа.
+  // Найти и вывести сумму всех чисел
+  public static void main(String[] args) {
+
+  }
+}
