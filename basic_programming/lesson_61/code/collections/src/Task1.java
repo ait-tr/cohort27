2e9f9b508e2c0162b04ca558c33b661a514c2586
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-16 19:49:26 +0200

    feat: evaluate Task1 efficiency

--- basic_programming/lesson_61/code/collections/src/Task1.java
+++ basic_programming/lesson_61/code/collections/src/Task1.java
@@ -4,17 +4,17 @@ public class Task1 {
 
   // Вводят количество чисел, а затем и сами числа.
   // Найти и вывести сумму всех чисел
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int size = scanner.nextInt();
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++) {
-      array[i] = scanner.nextInt();
+  public static void main(String[] args) {    // time = O(n)    space = O(n)
+    Scanner scanner = new Scanner(System.in); // O(1)           O(1)
+    int size = scanner.nextInt();             // O(1)           O(1)
+    int[] array = new int[size];              // O(1)           O(n)
+    for (int i = 0; i < size; i++) {          // n раз = O(n)   +O(1)
+      array[i] = scanner.nextInt();           // - O(1)
     }
-    int sum = 0;
-    for (int i = 0; i < array.length; i++) {
-      sum += array[i];
+    int sum = 0;                              // O(1)           O(1)
+    for (int i = 0; i < array.length; i++) {  // n раз = O(n)   +O(1)
+      sum += array[i];                        // - O(1)
     }
-    System.out.println(sum);
+    System.out.println(sum);                  // O(1)
   }
 }
