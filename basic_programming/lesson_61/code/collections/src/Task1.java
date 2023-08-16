e6a974596a16c5c8096a9effb3f197d95d0a6133
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-16 19:39:21 +0200

    feat: add Task1 solution

--- basic_programming/lesson_61/code/collections/src/Task1.java
+++ basic_programming/lesson_61/code/collections/src/Task1.java
@@ -5,6 +5,16 @@ public class Task1 {
   // Вводят количество чисел, а затем и сами числа.
   // Найти и вывести сумму всех чисел
   public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
+    int size = scanner.nextInt();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++) {
+      array[i] = scanner.nextInt();
+    }
+    int sum = 0;
+    for (int i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    System.out.println(sum);
   }
 }
