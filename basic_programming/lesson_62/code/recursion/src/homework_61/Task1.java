f8482d9e63b899e59ec61f35b50331dba6ada071
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-18 18:47:41 +0200

    feat: add Task1 solution

--- basic_programming/lesson_62/code/recursion/src/homework_61/Task1.java
+++ basic_programming/lesson_62/code/recursion/src/homework_61/Task1.java
@@ -1,10 +1,27 @@
 package homework_61;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task1 {
 
   // Найти минимальное чётное число в последовательности положительных чисел
   // или вывести -1, если такого числа не существует.
   public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
+    List<Integer> numbers = new ArrayList<>();
+    while (scanner.hasNextInt()) {
+      numbers.add(scanner.nextInt());
+    }
+    int min = numbers.get(0);
+    for (int x : numbers) {
+      if (x % 2 == 0) {
+        if (x < min) {
+          min = x;
+        }
+      }
+    }
+    System.out.println(min);
   }
 }
