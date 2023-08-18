6f27b0d30e4ed647499e5d4c15c2e2aa56ffbabb
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-18 19:35:07 +0200

    fix: remove ArrayList

--- basic_programming/lesson_62/code/recursion/src/homework_61/Task1.java
+++ basic_programming/lesson_62/code/recursion/src/homework_61/Task1.java
@@ -1,21 +1,16 @@
 package homework_61;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Task1 {
 
   // Найти минимальное чётное число в последовательности положительных чисел
   // или вывести -1, если такого числа не существует.
-  public static void main(String[] args) {        // Time=O(n)  Space=O(n)
+  public static void main(String[] args) {        // Time=O(n)  Space=O(1)
     Scanner scanner = new Scanner(System.in);     // O(1)       O(1)
-    List<Integer> numbers = new ArrayList<>();    // O(1)       O(1)
-    while (scanner.hasNextInt()) {                // n=O(n)     O(n)
-      numbers.add(scanner.nextInt());             // *O(1)      *O(1)
-    }
     int min = -1;                                 // O(1)       O(1)
-    for (int x : numbers) {                       // n=O(n)     O(1)
+    while (scanner.hasNextInt()) {                // n=O(n)     n=O(1)
+      int x = scanner.nextInt();                  // *O(1)      +O(1)
       if (x % 2 == 0) {                           // *O(1)
         // если до этого чётных не было (min == -1) или новое меньше предыдущего минимума (x < min)
         if (min == -1 || x < min) {               // *O(1)
