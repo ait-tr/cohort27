1410ec8fe4f89e9e7aa9f8f2ff4e27fc833e7321
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-18 19:14:35 +0200

    feat: evaluate homework_61.Task1 efficiency

--- basic_programming/lesson_62/code/recursion/src/homework_61/Task1.java
+++ basic_programming/lesson_62/code/recursion/src/homework_61/Task1.java
@@ -8,21 +8,21 @@ public class Task1 {
 
   // Найти минимальное чётное число в последовательности положительных чисел
   // или вывести -1, если такого числа не существует.
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    List<Integer> numbers = new ArrayList<>();
-    while (scanner.hasNextInt()) {
-      numbers.add(scanner.nextInt());
+  public static void main(String[] args) {        // Time=O(n)  Space=O(n)
+    Scanner scanner = new Scanner(System.in);     // O(1)       O(1)
+    List<Integer> numbers = new ArrayList<>();    // O(1)       O(1)
+    while (scanner.hasNextInt()) {                // n=O(n)     O(n)
+      numbers.add(scanner.nextInt());             // *O(1)      *O(1)
     }
-    int min = -1;
-    for (int x : numbers) {
-      if (x % 2 == 0) {
+    int min = -1;                                 // O(1)       O(1)
+    for (int x : numbers) {                       // n=O(n)     O(1)
+      if (x % 2 == 0) {                           // *O(1)
         // если до этого чётных не было (min == -1) или новое меньше предыдущего минимума (x < min)
-        if (min == -1 || x < min) {
-          min = x;
+        if (min == -1 || x < min) {               // *O(1)
+          min = x;                                // *O(1)
         }
       }
     }
-    System.out.println(min);
+    System.out.println(min);                      // O(1)
   }
 }
