38cca0eee969c124de97ecc5c62cfea074baa318
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-21 18:35:10 +0200

    feat: add homework_62.Task1 template

---
+++ basic_programming/lesson_63/code/anonymous/src/homework_62/Task1.java
@@ -0,0 +1,11 @@
+package homework_62;
+
+public class Task1 {
+
+  // Напишите рекурсивный метод `public static String reverse(int x)`,
+  // который будет возвращать строку, содержащую десятичные цифры числа в обратном порядке
+  // (развернёт число).
+  public static String reverse(int x) {
+    return "";
+  }
+}
