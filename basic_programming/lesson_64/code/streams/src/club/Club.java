dbc4737731790843839359e02ec4a39396c91e83
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-23 20:01:38 +0200

    feat: implement Club methods

--- basic_programming/lesson_64/code/streams/src/club/Club.java
+++ basic_programming/lesson_64/code/streams/src/club/Club.java
@@ -1,34 +1,64 @@
 package club;
 
+import java.util.ArrayList;
+
 public class Club {
 
   private final String title;
+  private final int capacity;
+  private final ArrayList<Visitor> visitors = new ArrayList<>();
 
-  public Club(String title) {
+  public Club(String title, int capacity) {
     this.title = title;
+    this.capacity = capacity;
   }
 
+  /**
+   * Вывод сообщения о начале работы клуба
+   */
   public void greet() {
-    System.out.println("Добро пожаловать в клуб " + title);
+    System.out.println("Добро пожаловать в клуб \"" + title + "\"");
   }
 
+  /**
+   * Проверка наличия свободного места
+   *
+   * @return <code>true</code> при наличии свободных мест
+   */
   public boolean hasFreeSpace() {
-    return true;
+    return visitors.size() < capacity;
   }
 
+  /**
+   * Извинение при отсутствии мест
+   */
   public void sorry() {
     System.out.println("Извините, мест больше нет");
   }
 
+  /**
+   * Попытка попасть в клуб
+   *
+   * @param visitor потенциальный посетитель
+   */
   public void tryToEnter(Visitor visitor) {
-
+    visitors.add(visitor);
   }
 
+  /**
+   * Вывод списка посетителей, отсортированного по возрасту в порядке возрастания
+   */
   public void printVisitorsByAge() {
-
+    System.out.println("В клуб \"" + title + "\" сейчас " + visitors.size() + " посетителей:");
+    for (Visitor v : visitors) {
+      System.out.println(v);
+    }
   }
 
+  /**
+   * Сообщение о закрытии клуба
+   */
   public void close() {
-    System.out.println("Клуб " + title + " закрывает свои двери до следующего раза");
+    System.out.println("Клуб \"" + title + "\" закрывает свои двери до следующего раза");
   }
 }
--- basic_programming/lesson_64/code/streams/src/club/ClubRunner.java
+++ basic_programming/lesson_64/code/streams/src/club/ClubRunner.java
@@ -16,7 +16,7 @@ public class ClubRunner {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    Club club = new Club("Название");
+    Club club = new Club("Название", 3);
     club.greet(); // начало работы, "добро пожаловать"
 
     while (hasNextVisitors(scanner)) {
