5b231edfe35360392c420e41f6ffd6d3f75ad7c3
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-23 20:09:05 +0200

    feat: implement Visitor methods

--- basic_programming/lesson_64/code/streams/src/club/Visitor.java
+++ basic_programming/lesson_64/code/streams/src/club/Visitor.java
@@ -4,7 +4,45 @@ import java.util.Scanner;
 
 public class Visitor {
 
+  private final String name;
+  private final int age;
+
+  public Visitor(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  @Override
+  public String toString() {
+    return "Visitor{" +
+        "name='" + name + '\'' +
+        ", age=" + age +
+        '}';
+  }
+
+  /**
+   * Чтение данных о посетителе клуба
+   * @param scanner источник данных
+   * @return посетитель
+   */
   public static Visitor interactiveRead(Scanner scanner) {
-    return new Visitor();
+    System.out.println("Как вас зовут?");
+    String name = scanner.nextLine();
+    System.out.println("Сколько вам лет?");
+    while (!scanner.hasNextInt()) {
+      System.out.println("Некорректный ввод: " + scanner.nextLine());
+      System.out.print("Введите целое число: ");
+    }
+    int age = scanner.nextInt();
+    scanner.nextLine(); // дочитываем строку после целого числа
+    return new Visitor(name, age);
   }
 }
