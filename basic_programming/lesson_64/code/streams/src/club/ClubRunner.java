d6b7c9106e8faf6b48c112d55c6ac246f3755fa7
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-23 20:37:14 +0200

    fix: correct yes/no processing

--- basic_programming/lesson_64/code/streams/src/club/ClubRunner.java
+++ basic_programming/lesson_64/code/streams/src/club/ClubRunner.java
@@ -8,10 +8,19 @@ public class ClubRunner {
 
   private static final Set<String> POSITIVE_ANSWERS = new HashSet<>();
   static {
+    POSITIVE_ANSWERS.add(""); // нажатая клавиша Enter - должен получиться ответ по умолчанию
     POSITIVE_ANSWERS.add("yes");
     POSITIVE_ANSWERS.add("y");
     POSITIVE_ANSWERS.add("да");
     POSITIVE_ANSWERS.add("д");
+  }
+
+  private static final Set<String> NEGATIVE_ANSWERS = new HashSet<>();
+  static {
+    NEGATIVE_ANSWERS.add("no");
+    NEGATIVE_ANSWERS.add("n");
+    NEGATIVE_ANSWERS.add("нет");
+    NEGATIVE_ANSWERS.add("н");
   }
 
   public static void main(String[] args) {
@@ -40,8 +49,12 @@ public class ClubRunner {
    * @return <code>true</code> при положительном ответе
    */
   private static boolean hasNextVisitors(Scanner scanner) {
-    System.out.println("Есть ли в очереди посетители? [y/N]");
+    System.out.println("Есть ли в очереди посетители? [Y/n]");
     String input = scanner.nextLine();
+    while (!POSITIVE_ANSWERS.contains(input) && !NEGATIVE_ANSWERS.contains(input)) {
+      System.out.print("Введите 'да' или 'нет': ");
+      input = scanner.nextLine();
+    }
     return POSITIVE_ANSWERS.contains(input.toLowerCase());
   }
 }
