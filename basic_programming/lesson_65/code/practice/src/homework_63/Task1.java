cb2be8e538819ab2b915f0c69f5a0ee370000469
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-25 19:05:05 +0200

    fix: replace equals with compareTo

--- basic_programming/lesson_65/code/practice/src/homework_63/Task1.java
+++ basic_programming/lesson_65/code/practice/src/homework_63/Task1.java
@@ -19,7 +19,7 @@ public class Task1 {
     books.sort(new Comparator<Book>() {
       @Override
       public int compare(Book o1, Book o2) {
-        if (!o1.getAuthor().equals(o2.getAuthor())) {
+        if (o1.getAuthor().compareTo(o2.getAuthor()) != 0) {
           return o1.getAuthor().compareTo(o2.getAuthor());
         }
         return o1.getTitle().compareTo(o2.getTitle());
