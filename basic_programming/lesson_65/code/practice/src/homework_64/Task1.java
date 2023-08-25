ea92151e315b061f20a6bb797e3df1c7a27f807e
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-25 19:36:15 +0200

    feat: add homework_64 solution

--- basic_programming/lesson_65/code/practice/src/homework_64/Task1.java
+++ basic_programming/lesson_65/code/practice/src/homework_64/Task1.java
@@ -1,5 +1,8 @@
 package homework_64;
 
+import homework_63.Book;
+import java.util.stream.Stream;
+
 public class Task1 {
 
   // Создайте список книг.
@@ -7,6 +10,17 @@ public class Task1 {
   // (при одинаковых авторах - по названиям)
   // и выведите получившийся список с использованием потоков.
   public static void main(String[] args) {
-    
+    Stream.of(
+        new Book("Другой Автор", "Другое Название", 25),
+        new Book("Автор", "Название", 25),
+        new Book("Автор", "Другое Название", 25),
+        new Book("Другой Автор", "Название", 25)
+    ).sorted((b1, b2) -> {
+      int authors = b1.getAuthor().compareTo(b2.getAuthor());
+      if (authors != 0) {
+        return authors;
+      }
+      return b1.getTitle().compareTo(b2.getTitle());
+    }).forEach(b -> System.out.println(b));
   }
 }
