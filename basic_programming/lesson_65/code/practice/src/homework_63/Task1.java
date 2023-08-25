51987a66ed7c211e1f9105251a85346fbbe56a12
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-25 18:58:00 +0200

    feat: add homework_63 solution

--- basic_programming/lesson_65/code/practice/src/homework_63/Task1.java
+++ basic_programming/lesson_65/code/practice/src/homework_63/Task1.java
@@ -1,10 +1,33 @@
 package homework_63;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Task1 {
 
   // Создайте список книг и отсортируйте книги по автору,
   // (при одинаковых авторах - по названиям)
   // при помощи анонимного класса в качестве компаратора.
   public static void main(String[] args) {
+    List<Book> books = new ArrayList<>();
+    books.add(new Book("Другой Автор", "Другое Название", 25));
+    books.add(new Book("Автор", "Название", 25));
+    books.add(new Book("Автор", "Другое Название", 25));
+    books.add(new Book("Другой Автор", "Название", 25));
+
+    books.sort(new Comparator<Book>() {
+      @Override
+      public int compare(Book o1, Book o2) {
+        if (!o1.getAuthor().equals(o2.getAuthor())) {
+          return o1.getAuthor().compareTo(o2.getAuthor());
+        }
+        return o1.getTitle().compareTo(o2.getTitle());
+      }
+    });
+
+    for (Book b : books) {
+      System.out.println(b);
+    }
   }
 }
