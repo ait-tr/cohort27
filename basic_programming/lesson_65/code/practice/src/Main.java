861dec809c90d3c0c1ee53845bf7eba15e6f6959
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-25 19:38:08 +0200

    feat: initial lesson commit

--- basic_programming/lesson_65/code/practice/src/Main.java
+++ basic_programming/lesson_65/code/practice/src/Main.java
@@ -1,3 +1,6 @@
+import books.Book;
+import java.util.stream.Stream;
+
 public class Main {
 
   // - домашнее задание
@@ -7,6 +10,17 @@ public class Main {
   // - тесты
   // - структура проекта
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Stream.of(
+        new Book("Другой Автор", "Другое Название", 25),
+        new Book("Автор", "Название", 25),
+        new Book("Автор", "Другое Название", 25),
+        new Book("Другой Автор", "Название", 25)
+    ).sorted((b1, b2) -> {
+      int authors = b1.getAuthor().compareTo(b2.getAuthor());
+      if (authors != 0) {
+        return authors;
+      }
+      return b1.getTitle().compareTo(b2.getTitle());
+    }).forEach(b -> System.out.println(b));
   }
 }
