1d9dd812bfaad3238d3bf9b535b9b647dad4b9d9
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-25 18:46:14 +0200

    feat: add lesson plan

--- basic_programming/lesson_65/code/practice/src/Main.java
+++ basic_programming/lesson_65/code/practice/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
 
+  // - домашнее задание
+  // - лямбда-выражения
+  // - статик
+  // - файлы
+  // - тесты
+  // - структура проекта
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
