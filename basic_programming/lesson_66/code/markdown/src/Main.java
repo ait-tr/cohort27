d77896584ef5365529c041e2cc1149366cb9a21c
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-08-28 18:55:49 +0200

    feat: add README

--- basic_programming/lesson_66/code/markdown/src/Main.java
+++ basic_programming/lesson_66/code/markdown/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
 
+  // Отличия критериев оценки от второго проекта:
+  // - комментарии в формате docstring, начинаются с /**, для каждого метода, кроме "простых"
+  // - файл LICENSE
+  // - файл README.md
+  // - эффективность программы
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
