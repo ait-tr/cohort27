6a52e85931aca4a6049081d39756d83d2914444d
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-09-04 20:54:35 +0200

    feat: add Main example

--- basic_programming/lesson_69/code/multithreading/src/Main.java
+++ basic_programming/lesson_69/code/multithreading/src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
+  // Parallelism (параллельность) - "как на нескольких компьютерах"
+  // Multithreading (concurrency) (многопоточность) - переключение между задачами
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Thread thread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        for (int i = 0; i < 50; ++i) {
+          System.out.println(i + " NEW");
+        }
+      }
+    });
+    thread.start();
+    for (int i = 0; i < 50; ++i) {
+      System.out.println(i + " MAIN");
+    }
   }
 }
