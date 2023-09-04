f509d8a4a4a28e7c37273a6641d17e6d60fc8933
Kirill Shnyrev <k.o.shnyrev@gmail.com>
2023-09-04 21:13:54 +0200

    feat: add Thread.join() example

--- basic_programming/lesson_69/code/multithreading/src/Main2.java
+++ basic_programming/lesson_69/code/multithreading/src/Main2.java
@@ -9,13 +9,14 @@ public class Main2 extends Thread {
   }
 
   // запустится, когда Main2 - основная программа
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     Thread thread = new Main2();
     Thread myThread = new MyThread(50);
-    thread.start();
-    myThread.start();
+    thread.start(); // запустить поток
     for (int i = 0; i < 50; ++i) {
       System.out.println(i + " MAIN");
     }
+    thread.join(); // подождать завершения потока
+    myThread.start();
   }
 }
