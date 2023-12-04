edaad2d018bd0fc2796ad1af4790bca530b4c32b
Andrey <k-udm@ya.ru>
2023-12-04 22:22:26 +0100

    project_shop

---
+++ back_end/project_shop/code/g_27_shop_project/src/test/java/de/aittr/g_27_shop_project/G27ShopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.aittr.g_27_shop_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G27ShopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
